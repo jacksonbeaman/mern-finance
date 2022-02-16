c019cce835bbaee6a89bb361d5d30e21bb4d7c3f
jacksonbeaman <jacksonbeaman@gmail.com>
2022-02-15 18:29:08 -0800

    Abstracts logic from App function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Amplify, { Auth } from 'aws-amplify';
+import axios from 'axios';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import HomeScreen from './screens/HomeScreen';
@@ -9,36 +10,38 @@ import RegisterScreen from './screens/RegisterScreen';
 import UserHeader from './components/userHeader/UserHeader';
 import QuoteScreen from './screens/QuoteScreen';
 
-const App = () => {
-  const [user, setUser] = useState(
-    localStorage.getItem(
+const lastUser = localStorage.getItem(
+  `CognitoIdentityServiceProvider.${process.env.REACT_APP_AWS_COGNITO_USER_POOL_WEB_CLIENT_ID}.LastAuthUser`
+)
+  ? localStorage.getItem(
       `CognitoIdentityServiceProvider.${process.env.REACT_APP_AWS_COGNITO_USER_POOL_WEB_CLIENT_ID}.LastAuthUser`
     )
-      ? localStorage.getItem(
-          `CognitoIdentityServiceProvider.${process.env.REACT_APP_AWS_COGNITO_USER_POOL_WEB_CLIENT_ID}.LastAuthUser`
-        )
-      : null
-  );
+  : null;
 
-  Amplify.configure({
-    Auth: {
-      // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
-      // identityPoolId: 'XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab',
+const amplifyConfig = {
+  Auth: {
+    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+    // identityPoolId: 'XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab',
 
-      // REQUIRED - Amazon Cognito Region
-      region: process.env.REACT_APP_AWS_COGNITO_REGION,
+    // REQUIRED - Amazon Cognito Region
+    region: process.env.REACT_APP_AWS_COGNITO_REGION,
 
-      // OPTIONAL - Amazon Cognito User Pool ID
-      userPoolId: process.env.REACT_APP_AWS_COGNITO_USER_POOL_ID,
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: process.env.REACT_APP_AWS_COGNITO_USER_POOL_ID,
 
-      // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-      userPoolWebClientId:
-        process.env.REACT_APP_AWS_COGNITO_USER_POOL_WEB_CLIENT_ID,
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId:
+      process.env.REACT_APP_AWS_COGNITO_USER_POOL_WEB_CLIENT_ID,
 
-      // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
-      mandatorySignIn: false,
-    },
-  });
+    // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+    mandatorySignIn: false,
+  },
+};
+
+const App = () => {
+  const [user, setUser] = useState(lastUser);
+
+  Amplify.configure(amplifyConfig);
 
   const signUp = async ({ username, password }) => {
     try {
@@ -71,11 +74,26 @@ const App = () => {
     }
   };
 
+  // const getQuote = async (symbol) => {
+  //   try {
+  //     const { data } = await axios.get(
+  //       `https://c5un9qkyu2.execute-api.us-west-2.amazonaws.com/prod?symbol=${symbol}`
+  //     );
+  //     res.json({
+  //       symbol: data.symbol,
+  //       companyName: data.companyName,
+  //       price: data.latestPrice,
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
   console.log(user);
 
   // You can get the current config object
   const currentConfig = Auth.configure();
-  console.log(currentConfig);
+  console.log(currentConfig.signInUserSession.idToken.jwtToken);
 
   return (
     <>
