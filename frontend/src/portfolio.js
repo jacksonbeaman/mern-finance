2908ff35e55076bfeecb01d67e9bd51bf5c708d5
jacksonbeaman <jacksonbeaman@gmail.com>
2022-03-17 17:06:41 -0700

    Abstracts test data to external import

---
+++ frontend/src/portfolio.js
@@ -0,0 +1,35 @@
+const portfolio = {
+  positions: [
+    {
+      id: 1,
+      symbol: 'MSFT',
+      companyName: 'Microsoft Corporation',
+      shares: 20,
+      transactionId: 1,
+    },
+    {
+      id: 2,
+      symbol: 'AAPL',
+      companyName: 'Apple Inc',
+      shares: 15,
+      transactionId: 2,
+    },
+    {
+      id: 3,
+      symbol: 'CAT',
+      companyName: 'Caterpillar Inc.',
+      shares: 25,
+      transactionId: 3,
+    },
+    {
+      id: 4,
+      symbol: 'JNJ',
+      companyName: 'Johnson & Johnson',
+      shares: 10,
+      transactionId: 4,
+    },
+  ],
+  cash: 15000,
+};
+
+export default portfolio;
--- frontend/src/screens/SellScreen.js
+++ frontend/src/screens/SellScreen.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Button from '../components/button/Button';
+import portfolio from '../portfolio';
 
 const SellScreen = ({
   onGetQuote,
@@ -8,26 +9,6 @@ const SellScreen = ({
 }) => {
   const [inputSymbol, setInputSymbol] = useState('');
   const [inputShares, setInputShares] = useState('');
-
-  const portfolio = {
-    positions: [
-      {
-        id: 1,
-        symbol: 'MSFT',
-        companyName: 'Microsoft Corporation',
-        price: 294.95,
-        shares: 20,
-      },
-      {
-        id: 2,
-        symbol: 'AAPL',
-        companyName: 'Apple Inc',
-        price: 163.2,
-        shares: 20,
-      },
-    ],
-    cash: 5000,
-  };
 
   const submitHandler = (e) => {
     e.preventDefault();
