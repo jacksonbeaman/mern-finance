7ed1053e147ac19a2a547f9ed5f0b25bc52e91f1
jacksonbeaman <jacksonbeaman@gmail.com>
2022-04-14 18:58:22 -0700

    Removes props for local state, imports fetch from utils, and adds view from total account value

--- src/screens/UserHomeScreen.js
+++ src/screens/UserHomeScreen.js
@@ -1,78 +1,27 @@
 import { useState, useEffect, useCallback } from 'react';
 import portfolio from '../portfolio.js';
+import { getQuote } from '../utils/fetches.js';
 
-const UserHomeScreen = ({ onGetQuote, quote, error }) => {
-  const [prices, setPrices] = useState([]);
-
-  // const getFakePrice = async (index) => {
-  //   try {
-  //     const settings = {
-  //       url: `/users/1/albums`,
-  //       baseURL: 'https://jsonplaceholder.typicode.com/',
-  //       method: 'get',
-  //       timeout: 0,
-  //     };
-  //     const { data } = await axios(settings);
-
-  //     return data[index];
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  // const getId = async (index) => {
-  //   try {
-  //     const { id } = await getFakePrice(index);
-  //     return id;
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  // const getPrices = useCallback(
-  //   () => portfolio.positions.map(({ symbol }, index) => symbol),
-  //   []
-  // );
-
-  // const getIds = () =>
-  //   portfolio.positions.map(async (position, index) => {
-  //     try {
-  //       const newId = await getId(index);
-  //       console.log(newId);
-  //       return newId;
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   });
-
-  // useEffect(() => {
-  //   const calcPrices = getPrices();
-  //   console.log(getId(5));
-  //   console.log(calcPrices);
-  //   setPrices(calcPrices);
-  //   console.log(getIds());
-  // }, [getPrices]);
-
-  const getPrices = useCallback(
-    () =>
-      portfolio.positions.map(async ({ symbol, shares }) => {
-        const { price } = await onGetQuote(symbol);
-        return price * shares;
-      }),
-    [onGetQuote]
-  );
+const UserHomeScreen = ({ userToken }) => {
+  const [positionValues, setPositionValues] = useState({});
 
   useEffect(() => {
-    (async () => {
-      try {
-        const calcPrices = await getPrices();
-        console.log(calcPrices);
-        setPrices(calcPrices);
-      } catch (error) {
-        console.error(error);
-      }
-    })();
-  }, [getPrices]);
+    const getPositionValues = () => {
+      portfolio.positions.forEach(async ({ symbol, shares }) => {
+        try {
+          const { price } = await getQuote(symbol, userToken);
+
+          setPositionValues((positionValues) => ({
+            ...positionValues,
+            [symbol]: price * shares,
+          }));
+        } catch (error) {
+          console.error(error);
+        }
+      });
+    };
+    getPositionValues();
+  }, [userToken]);
 
   return (
     <>
@@ -94,14 +43,29 @@ const UserHomeScreen = ({ onGetQuote, quote, error }) => {
                   <td>{position.symbol}</td>
                   <td>{position.companyName}</td>
                   <td>{position.shares}</td>
-                  <td>{prices[index]}</td>
+                  <td>${positionValues[position.symbol]}</td>
                 </tr>
               ))}
               <tr>
                 <td>CASH</td>
                 <td></td>
                 <td></td>
-                <td>Reduce() Total</td>
+                <td>${portfolio.cash}</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                  $
+                  {positionValues &&
+                    Object.values(positionValues)
+                      .reduce(
+                        (acc, positionValue) => acc + positionValue,
+                        portfolio.cash
+                      )
+                      .toFixed(2)}
+                </td>
               </tr>
             </tbody>
           </table>
