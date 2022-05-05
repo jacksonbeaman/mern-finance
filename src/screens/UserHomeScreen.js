35bb8e19a144f006a025074d3b593cd978ba3874
jacksonbeaman <jacksonbeaman@gmail.com>
2022-05-05 14:56:55 -0700

    Removes unused useCallback function from UserHomeScreen

--- src/screens/UserHomeScreen.js
+++ src/screens/UserHomeScreen.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import portfolio from '../portfolio.js';
 import { getQuote } from '../utils/fetches.js';
 
